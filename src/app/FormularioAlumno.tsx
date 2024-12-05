569c948727a7397d870f68aad453b493be9f069d
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-12-05 12:26:37 -0300

    jueves

--- src/app/FormularioAlumno.tsx
+++ src/app/FormularioAlumno.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react'
 
 export const FormularioAlumno = () => {
@@ -7,15 +8,23 @@ export const FormularioAlumno = () => {
     const [correo, setCorreo] = useState("")
   return (
     <>
+    <p>Nombre:{nombre}</p>
+    <p>Apellido:{apellido}</p>
+    <p>Edad:{edad}</p>
+    <p>Correo:{correo}</p>
     <form>
         <label>Nombre</label><br></br>
-        <input type='text' placeholder='Ingrese nombre'/><br/>
+        <input type='text' placeholder='Ingrese nombre'
+        onClick={(e)=>{setNombre(e.currentTarget.value)}}/><br/>
         <label>Apellido</label><br></br>
-        <input type='text' placeholder='Ingrese Apellido'/><br/>
+        <input type='text' placeholder='Ingrese Apellido'
+        onClick={(e)=>{setApellido(e.currentTarget.value)}}/><br/>
         <label>Edad</label><br></br>
-        <input type='number' placeholder='Ingrese Edad'/><br/>
-        <label>Nombre</label><br/>
-        <input type='email' placeholder='Ingrese correo'/><br/>
+        <input type='number' placeholder='Ingrese Edad'
+        onClick={(e)=>{setEdad(e.currentTarget.value)}}/><br/>
+        <label>Correo</label><br/>
+        <input type='email' placeholder='Ingrese correo'
+        onClick={(e)=>{setCorreo(e.currentTarget.value)}}/><br/>
         <button type='button'>Registrar</button>
     </form>
     </>
