3f9ba2c4ddf430def5a881335c911bc2a197f669
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-12-18 13:48:31 -0300

    miercoles

---
+++ src/app/TablaAlumnos.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const TablaAlumnos = () => {
+  return (
+    <>
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Edad</th>
+                <th>Correo</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+
+    </>
+  )
+}
+export default TablaAlumnos
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import FormularioAlumno from "./FormularioAlumno";
 import FormularioAlumnoFinal from "./FormularioAlumnoFinal";
+import TablaAlumnos from "./TablaAlumnos";
 
 export default function Home() {
   return (
     <>
-    <h1>Hola</h1>
-    <p
-     style={{backgroundColor:'red'}} >
-      Come estas</p>
-      <FormularioAlumno></FormularioAlumno>
-      <FormularioAlumnoFinal/>
-    <input type="checkbox"
-     className="PintarAzul"
-    
-    />
+   <TablaAlumnos/>
     </>
   );
 }
