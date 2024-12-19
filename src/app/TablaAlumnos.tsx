58222fe5a29b551980d371e80c41595a79ef3ec9
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-12-19 12:04:59 -0300

    jueves

--- src/app/TablaAlumnos.tsx
+++ src/app/TablaAlumnos.tsx
@@ -2,6 +2,9 @@
 import { obtenerAlumnos } from '@/Firebase/Promesas'
 import { IAlumno } from '@/Interfaces/IAlumno'
 import React, { useEffect, useState } from 'react'
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export const TablaAlumnos = () => {
     const [IAlumnos, setLAlumnos] = useState<IAlumno[]>([])
@@ -20,13 +23,14 @@ export const TablaAlumnos = () => {
     },[])
   return (
     <>
-    <table>
+    <Table>
         <thead>
             <tr>
                 <th>Nombre</th>
                 <th>Apellido</th>
                 <th>Edad</th>
                 <th>Correo</th>
+                <th>Accion</th>    
             </tr>
         </thead>
         <tbody>
@@ -37,14 +41,35 @@ export const TablaAlumnos = () => {
                  <td>{alumno.apellido}</td>
                  <td>{alumno.edad}</td>
                  <td>{alumno.correo}</td>
+                 <td>
+                    <Button variant='warning'>Actualizar</Button>
+                    <Button variant='danger'>Eliminar</Button>
+                 </td>
                 </tr>
                 
             )})
             }
         </tbody>
-    </table>
+    </Table>
 
-    </>
+       {
+        IAlumnos.map((alumno)=>{return(
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>{alumno.nombre} {alumno.apellido}</Card.Title>
+              <Card.Text>
+                <h6>Datos</h6>
+                <p>Edad:{alumno.edad}</p>
+              </Card.Text>
+              <Button variant="warning">Editar</Button>
+              <Button variant="danger">Eliminar</Button>
+            </Card.Body>
+          </Card>
+        )})
+    }
+
+    </> 
   )
 }
 export default TablaAlumnos
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import styles from "./page.module.css";
 import FormularioAlumno from "./FormularioAlumno";
 import FormularioAlumnoFinal from "./FormularioAlumnoFinal";
 import TablaAlumnos from "./TablaAlumnos";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
     <>
+
    <TablaAlumnos/>
     </>
   );
